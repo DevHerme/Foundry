70f83a7898f0be2a1fea13457fb72db833a4a364
DevHerme <andyhermanson00@gmail.com>
2025-04-04 11:49:53 -0500

    test: Testing commit

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function DashboardPage({ onLogout }) {
     return (
         <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
-            <h1 className="text-3xl font-bold mb-4">Welcome to Your Dashboardz!</h1>
+            <h1 className="text-3xl font-bold mb-4">Welcome to Your Dashboard!s</h1>
             <button
                 onClick={onLogout}
                 className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
